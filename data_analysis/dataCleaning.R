32aefde1dfa9d40c75b1394343530b2867172b8d
shreyasdraut <raut_shreyas@yahoo.co.in>
2015-07-07 00:18:14 -0400

    R script

---
+++ data_analysis/dataCleaning.R
@@ -0,0 +1,137 @@
+getwd()
+setwd('/Users/shreyas/Desktop/SARBayes')
+library(XLConnect)
+
+removeData<- function (data){
+  newData<-subset(data, !Subject.Category %in% c('Aircraft-nonpowered','Aircraft','Abduction' ) )
+  newData$Super.Category<-
+    ifelse( newData$Subject.Category %in% c('Fisher','Boater','boater','Swimmer','Diver'), 'Water', 
+            
+            ifelse(newData$Subject.Category %in%  c('Vehicle','Vehicle-4wd','Motorcycle','Vehicle-Left','Mountain Biker','ATV'), 'Vehicle',
+                   
+                   ifelse(  newData$Subject.Category %in% c('Skier-Alpine','Snowboarder','Skier-Nordic','Snowmobile','Skier','Snowshoer','Ice Skater'), 'Snow',
+                            
+                            ifelse(newData$Subject.Category =='Child', 'Child',
+                                   
+                                   ifelse(newData$Subject.Category =='Despondent', 'Despondent',
+                                          
+                                          ifelse(newData$Subject.Category %in%  c('Mental Retardation','Autistic','Substance Abuse','Psychotic','Dementia'), 'Other Mental issues',
+                                                 
+                                                 ifelse(newData$Subject.Category =='Dementia', 'Dementia', 
+                                                        
+                                                        ifelse(is.na(newData$Subject.Category) , 'uncategorized', 'outDoor')
+                                                 )
+                                          )
+                                   )
+                            )
+                   )
+            )
+    )
+  
+  
+  
+  #identifying Group
+  newData$Group<-ifelse(newData$Number.Lost>1|newData$Group.Type %in% c("AC","MM","FF","MF"),'Yes','No')
+  newData$Group<-ifelse(newData$Group.Type %in% c("F","M"),newData$Group,"Yes")
+  #newData$Group<-ifelse(newData$Number.Lost>1,'Yes','No')
+  class(newData$Age)<-"numeric"
+  class(newData$Total.hours)
+  newData$Total.hours<-round(newData$Total.hours, digits = 2)
+  newData$Total.hours<-newData$Total.hours*24
+  
+  #newData$Total.hours<-format(newData$Total.hours, format = "%H:%M:%S")
+  
+  
+  refinedData <- newData
+  
+  refinedData <- refinedData[, -which(names(refinedData) %in% 
+                                        c("Key.","Incident.Date","Data.Source","Key.  ","Mission..","City",
+                                          "County","EcoRegion.Division",
+                                          "Subject.Sub.Category","Clothing","Survival.training","Notify.hours",
+                                          "Search.hours","LKP.Type","LKP.Coord...N.S.","LKP.Coord...E.W.",
+                                          "Destination.Coord...N.S.","Destination.Coord...E.W.","Wind..kph.",
+                                          "Investigative.Find","Suspended","Medical","Subject.Found.feature",
+                                          "Found.Secondary","Find.Resource	Detection","Responsivenss",
+                                          "Lost.Strategy","Elevation.Change..ft.","Mobile..hours.",
+                                          "Distance.IPP..km.","Distance.IPP..miles.","Distance.Invest...km.",
+                                          "Distance.Invest...miles.","Track.Offset..m.","Find.Coord..N.S.",
+                                          "Find.Coord..E.W.","Dispersion.Angle..ÃŽ..Ã‚.","Mission.Close",
+                                          "Mission.Cause","Rescue.Method","Total.Air.Hours","Total.Dog.Hours",
+                                          "Total.Personnel","Total.Man.Hours","Total.Cost","Mission.Contact",
+                                          "Mission.Contact..","Comments","Subject.Activity",
+                                          "Number.Lost","Search.Outcome","Find.Resource","Detection",
+                                          "Mobility","Subject.Category","Dispersion.Angle..Î..Â.","Sex",
+                                          "Weather","Snow"	,"Rain"))]
+  
+  
+ # kept population density(,"Population.Density") and  scenario (,"Scenario")
+  #build can be removed
+  refinedData<-subset(refinedData, Incident.Type %in% c('Search','Water')|is.na(Incident.Type))
+  #refinedData<-subset(refinedData, Incident.Type %in% c('Search','Water'))
+  
+  
+  refinedData$Subject.Status<-gsub("No Trace","DOA", refinedData$Subject.Status)
+  #following statement may not be needed
+  refinedData$Subject.Status<-gsub("Well","Alive", refinedData$Subject.Status)
+  #changed at 11 jun 3:35pm, following statement may not be needed
+  refinedData$Subject.Status<-gsub("Injured","Alive", refinedData$Subject.Status)
+  #gsub("Injured", "Alive", refinedData$Subject.Status, ignore.case = TRUE, perl = FALSE,fixed = FALSE, useBytes = FALSE)
+  
+  
+  keep<-refinedData$Subject.Status %in% c('DOA','Alive') | is.na(refinedData$Subject.Status)
+  summary(keep)
+  refinedData<-data.frame(lapply(refinedData, function(v) {
+    if (is.character(v)) return(toupper(v))
+    else return(v)
+  }))
+  #
+  
+  
+  refinedData$Experience<-gsub("EXPERIENCE","GOOD", refinedData$Experience)
+  refinedData$Personality<-gsub("SUICIDAL","WITHDRAWN", refinedData$Personality)
+  
+  
+  groupDOA<-grepl("DOA", refinedData$Subject.Status, ignore.case = TRUE,fixed = FALSE, useBytes = FALSE)
+  groupAlive<-grepl("ALIVE", refinedData$Subject.Status, ignore.case = TRUE,fixed = FALSE, useBytes = FALSE)
+  refinedData$Subject.Status<-ifelse(groupDOA,"DOA",ifelse(groupAlive,"ALIVE",NA))
+  refinedData$Group= factor(refinedData$Group)
+  summary(refinedData$Group)
+  
+  xy<-is.na(refinedData$Subject.Status)
+  refinedData$Subject.Status<-ifelse(xy,"ALIVE",refinedData$Subject.Status)
+  
+  
+  
+
+  refinedData$Subject.Status= factor(refinedData$Subject.Status)
+  class(refinedData$Age)<-"numeric"
+  class(refinedData$Total.hours)<-"numeric"
+  refinedData$EcoRegion.Domain=factor(refinedData$EcoRegion.Domain)
+  refinedData$Terrain= factor(refinedData$Terrain)
+#  refinedData$Subject.Category= factor(refinedData$Subject.Category)
+  refinedData$Incident.Type=  factor(refinedData$Incident.Type)
+ 
+ 
+ #(,"Population.Density") and  scenario (,"Scenario")
+  refinedData$Population.Density= factor(refinedData$Population.Density)
+  refinedData$Scenario=  factor(refinedData$Scenario)
+ 
+  refinedData$Group.Type = factor(refinedData$Group.Type)
+  refinedData$Build = factor(refinedData$Build)
+  refinedData$Physical.Fitness = factor(refinedData$Physical.Fitness)
+  refinedData$Mental.Fitness = factor(refinedData$Mental.Fitness)
+  refinedData$Experience = factor(refinedData$Experience)
+  refinedData$Equipment = factor(refinedData$Equipment)
+  refinedData$Personality = factor(refinedData$Personality)
+return(refinedData)
+}
+
+
+Data <- readWorksheet(loadWorkbook("ISRIDclean.xls"),sheet=1)
+summary(Data)
+str(Data)
+finalData<-removeData(Data)
+
+library(foreign)
+write.csv(finalData, file = "ISRIDfinalData.csv")
+write.arff(finalData, "finalData.arff", eol = "\n", relation = deparse(substitute(finalData)))
